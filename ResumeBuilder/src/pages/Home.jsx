f135aedba758d2ca83bbc71a1016fd89c8e5c6fc
Abhisek Singh <abhisek.singh@stu.adamasuniversity.ac.in>
2026-02-23 18:03:09 +0530

    Fix: Hard reset file casing for TemplatesSection

--- ResumeBuilder/src/pages/Home.jsx
+++ ResumeBuilder/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
 import Testimonial from '../components/home/Testimonial'
 import Footer from '../components/home/Footer'
-import TemplatesSection from './Templatessection'
+import TemplatesSection from './TemplatesSection'
 import Contact from './Contact'
 
 const Home = () => {
